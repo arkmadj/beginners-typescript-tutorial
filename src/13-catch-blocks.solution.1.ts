f8fa68d51ca00d98a1e83bb847989847704d3fac
arkmadj <ahmadjinadu@gmail.com>
2023-07-22 08:22:16 +0100

    added instance of Error

--- src/13-catch-blocks.solution.1.ts
+++ src/13-catch-blocks.solution.1.ts
@@ -1,15 +1,17 @@
 import { expect, it } from "vitest";
 
 const tryCatchDemo = (state: "fail" | "succeed") => {
-  try {
-    if (state === "fail") {
-      throw new Error("Failure!");
-    }
-  } catch (e: any) {
-    return e.message;
-  }
+	try {
+		if (state === "fail") {
+			throw new Error("Failure!");
+		}
+	} catch (e: any) {
+		if (e instanceof Error) {
+			return e.message;
+		}
+	}
 };
 
 it("Should return the message when it fails", () => {
-  expect(tryCatchDemo("fail")).toEqual("Failure!");
+	expect(tryCatchDemo("fail")).toEqual("Failure!");
 });
